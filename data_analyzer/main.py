4a2dbae60ae781139771200f41b0cf002f72b5b0
Abraham Dick <abedick@ku.edu>
2017-09-16 11:45:08 -0500

    Basic control updated

--- data_analyzer/main.py
+++ data_analyzer/main.py
@@ -2,15 +2,24 @@
 #   File: main.py
 #   Author: Abraham Dick
 #   Date Created: 09/16/2017
-#   
+#   Description: In charge of setting up data aquisition program and printing the application logo
 #
 
 from src.driver import Driver
 
-def main():
-    drive = Driver(80)
+import os
 
+# Version Information
+version = "Alpha 0.00.034"
+
+def main():
+    drive = Driver(100,version)
+
+    os.system("clear")
     drive.logo_print()
+
+    drive.title_print_ext("Data Analyzer for Baja","Make a selection")
+
 
 
 main()
--- data_analyzer/src/driver.py
+++ data_analyzer/src/driver.py
@@ -8,9 +8,11 @@
 class Driver :
 
     __line_length = 50
+    __version = None
 
-    def __init__(self,length) :
+    def __init__(self,length,version) :
         self.__line_length = length
+        self.__version = version
 
     def logo_print(self) :
         print("""   __              _                    _                   _                                  _
@@ -20,3 +22,75 @@ class Driver :
  \___/ \__,_|\__, |_| |_|\__,_| \_/\_/ |_|\_\ \/    \/\___/ \__\___/|_|  |___/ .__/ \___/|_|   \__|___/ 
               |___/                                                           |_|""")
 
+        print("\nDeveloped by Abe Dick, Version",self.__version)
+
+    def title_print(self,title):
+        title_size = len(title)
+
+        # top line
+        print("\n\u250C", end='')
+        for i in range(0,self.__line_length):
+            print("\u2500", end='')
+        print("\u2510")
+
+        # title line
+        print("\u2502 \u25B6",title,end='')
+        for i in range(self.__line_length-7-title_size):
+
+            if i == ((self.__line_length-title_size)-9):
+                print("[JMS]",end='')
+            else :
+                print(" ",end='')
+        print("\u2502")
+
+        # bottom line
+        print("\u2514",end='')
+        for i in range(self.__line_length):
+            print("\u2500",end='')
+        print("\u2518")
+    
+    def title_print_ext(self,title,sub):
+        title_size = len(title)
+
+        # top line
+        print("\n\u250C", end='')
+        for i in range(self.__line_length):
+            print("\u2500", end='')
+        print("\u2510")
+
+        # title line
+        print("\u2502 \u25B6",title,end='')
+        for i in range(self.__line_length-7-title_size):
+
+            if i == ((self.__line_length-title_size)-9):
+                print("[JMS]",end='')
+            else :
+                print(" ",end='')
+        print("\u2502")
+
+        # middle line
+        print("\u2514",end='')
+
+        for i in range(0,self.__line_length):
+            if i == 3:
+                print("\u252C",end='')
+            else:
+                print("\u2500",end='')
+        print("\u2534\u2500\u2500\u2500\u2510")
+
+        # sub title line
+
+        sub_size = len(sub)
+        print("    \u2502 \u25B6 ",sub,end='')
+        for i in range(self.__line_length - (sub_size+4)):
+            if i == (self.__line_length-7 - sub_size):
+                print("\u2517",end='')
+            else:
+                print(' ',end='')
+        print("\u2502")
+
+        # bottom line
+        print("    \u2514",end='')
+        for i in range(self.__line_length):
+            print("\u2500",end='')
+        print("\u2518")
